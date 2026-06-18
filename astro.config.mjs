313ca38b51212a0b70acf5af46fd065f21a1bf14
eppic <ncraft121@gmail.com>
2026-06-18 16:53:37 +0200

    added base, site options

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    base: "/",
+    site: "https://kaeswurm.de/",
     i18n: {
         locales: ["de", "en"],
         defaultLocale: "de",
