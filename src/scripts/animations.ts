73f94e78e5534be3bee39b7b2fc05211a4aac9e5
eppic <ncraft121@gmail.com>
2026-07-08 20:42:15 +0200

    Added new animations to Header, renamed animation classes

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -2,9 +2,10 @@ import { animate, stagger, inView } from "motion";
 
 export function basicAnimations() {   
 
-    // Default Animation
+    inView
+
     inView(
-        ".fade-in",
+        ".fade-up",
         (e) => {
             animate(e, { opacity: [0, 1], y: [10, 0] }, { ease: [0.39, 0.24, 0.3, 1], duration: 1 });
 
@@ -13,17 +14,28 @@ export function basicAnimations() {
 
     )
 
-    // Stagger Animation
     inView(
-        ".fade-in-stagger",
+        ".fade-up-stagger",
         (e) => {
             const sub = e.querySelectorAll(".stagger");
             if (sub.length > 0) {
-                animate(sub, { opacity: [0, 1], y: [10, 0], }, { ease: [0.39, 0.24, 0.3, 1], duration: 1, delay: stagger(0.10, { startDelay: 0.10 }) } );
+                animate(sub, { opacity: [0, 1], y: [10, 0], }, { ease: [0.39, 0.24, 0.3, 1], duration: 1, delay: stagger(0.20, { startDelay: 0.10 }) } );
             }
         },
         { amount: 0.25 }
 
     )
+
+    inView(
+        ".fade-left-delay-stagger",
+        (e) => {
+            const sub = e.querySelectorAll(".stagger");
+            if (sub.length > 0) {
+                animate(sub, { opacity: [0, 1], x: [10, 0], }, { ease: [0.39, 0.24, 0.3, 1], duration: 1, delay: stagger(0.20, { startDelay: 0.33 }) } );
+            }
+        },
+    )
+
+
 }
 
